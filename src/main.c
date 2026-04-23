6bc3973fb2769c280c5171296e2f87cbb8b5072a
Batorshin Artem <NorvellArt@gmail.com>
2026-04-23 22:23:38 +0300

    set up example ws

--- src/main.c
+++ src/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <ws.h>
+
+void onopen(ws_cli_conn_t client)
+{
+    char *cli;
+    cli = ws_getaddress(client);
+    printf("Connection opened, addr: %s\n", cli);
+}
+
+void onclose(ws_cli_conn_t client)
+{
+    char *cli;
+    cli = ws_getaddress(client);
+    printf("Connection closed, addr: %s\n", cli);
+}
+
+void onmessage(ws_cli_conn_t client,
+    const unsigned char *msg, uint64_t size, int type)
+{
+    char *cli;
+    cli = ws_getaddress(client);
+    printf("I receive a message: %s (%zu), from: %s\n", msg,
+        size, cli);
+
+    sleep(2);
+    ws_sendframe_txt(client, "hello");
+    sleep(2);
+    ws_sendframe_txt(client, "world");
+}
 
 int main()
 {
-    printf("Works\n");
-    return 0;
+    ws_socket(&(struct ws_server){
+        .host = "localhost",
+        .port = 7777,
+        .thread_loop   = 0,
+        .timeout_ms    = 1000,
+        .evs.onopen    = &onopen,
+        .evs.onclose   = &onclose,
+        .evs.onmessage = &onmessage
+    });
+
+    return (0);
 }
